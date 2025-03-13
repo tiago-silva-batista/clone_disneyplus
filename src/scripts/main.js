3a8ecf106300aef9ba95464911a7aa091e7398d2
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-03-13 19:49:55 -0300

    atualização

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (posicaoAtual < alturaHero) {
             ocultaElementosDoHeader();
         } else {
-            exibeElemntosDoHeader()
+            exibeElemntosDoHeader();
         }
     })
 
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
             escondeTodasAbas();
-            abaAlvo.classList.add('shows__list--is-active');
+            aba.classList.add('shows__list--is-active')
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
         })
@@ -59,9 +59,9 @@ function removeBotaoAtivo() {
 }
 
 function escondeTodasAbas() {
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const tabsContainer = document.querySelectorAll('[data-tab-id]')
     
     for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is-active');
+        tabsContainer[i].classList.remove('shows__list--is-active')
     }
 }
